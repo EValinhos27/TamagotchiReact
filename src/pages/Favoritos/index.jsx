5f849549fa5463112a92771a6deb55b01634e6b5
caiolukas-residencia <caio.rocha@residente.serratec.org>
2026-06-29 04:26:25 -0300

    fix: Remoção de import inutilizado

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import logoTamagotchi from '../../assets/logoTamagotchi.png';
-
 import * as S from './style'
 
 const Favorites = () => {
